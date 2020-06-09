d8a59c25d4f8f0bb4e531b1aca273ba885696899
kraljan12 <kraljan12@gmail.com>
2020-06-09 14:05:48 +0200

    Add files via upload

--- Rozetyxzavesy/src/Rozetyxzavesy/Graphics.java
+++ Rozetyxzavesy/src/Rozetyxzavesy/Graphics.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -13,12 +14,12 @@ public class Graphics{
 	Container con;
 	JPanel leftNamePanel;
 	JLabel leftNameLabel;
-	
 	JPanel rightNamePanel;
 	JLabel rightNameLabel;
-	
 	JPanel resultNamePanel;
 	JLabel resultNameLabel;
+	JLabel logo;
+	ImageIcon icon;
 	Font rzFont = new Font("Arial" , Font.BOLD, 75); 
 	Font resultFont = new Font("Arial" , Font.BOLD, 300);
 	
@@ -26,45 +27,41 @@ public class Graphics{
 		window = new JFrame();
 		window.setSize(1920, 1080);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		if(result < 60.0 ) {
-			window.getContentPane().setBackground(Color.red);
-		}
-		
-		else {
-			window.getContentPane().setBackground(Color.green);
-		}
-		
+		window.getContentPane().setBackground(Color.white);
 		//window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		//window.setUndecorated(true);
 		window.setLayout(null);
 		con = window.getContentPane();
 		//window.setVisible(true);
 	}
+	
+	public void logo() {
+	  logo = new JLabel();
+	  ImageIcon icon = new ImageIcon("logo.jpg");
+	  logo = new JLabel(icon);
+	  logo.setBounds(1500, 300, 100, 82); //x,y,size x , size y
+	  con.add(logo);
+	}
+	
 	public void leftChange(float leftNum) {	
 		leftNamePanel = new JPanel();
 		leftNamePanel.setBounds(50, 100, 600, 100); //x,y,size x , size y
 		leftNamePanel.setBackground(Color.white);
-		
-		leftNameLabel = new JLabel ("ROZETY : " + leftNum);
+		leftNameLabel = new JLabel ("ZÁVÌSY : " + leftNum);
 		leftNameLabel.setForeground(Color.black);
 		leftNameLabel.setFont(rzFont);
-		
 		leftNamePanel.add(leftNameLabel);
 		con.add(leftNamePanel);	
 		leftNamePanel.setVisible(true);
 		//window.setVisible(true);
 	}
 	public void rightChange(float rightNum) {
-		
-		
 		rightNamePanel = new JPanel();
 		rightNamePanel.setBounds(50, 900, 600, 100); //x,y,size x , size y
 		rightNamePanel.setBackground(Color.white);
-		
-		rightNameLabel = new JLabel ("ZÁVÌSY : " + rightNum);
+		rightNameLabel = new JLabel ("ROZETY : " + rightNum);
 		rightNameLabel.setForeground(Color.black);
 		rightNameLabel.setFont(rzFont);
-		
 		rightNamePanel.add(rightNameLabel);
 		con.add(rightNamePanel);	
 		rightNamePanel.setVisible(true);
@@ -72,24 +69,19 @@ public class Graphics{
 		//return rightChangeNum;
 	}
 	public void result(float result) {
-
 		String formattedResult = String.format("%.02f", result);
-		
 		resultNamePanel = new JPanel();
-		resultNamePanel.setBounds(200, 400, 1500, 350); //x,y,size x , size y
+		resultNamePanel.setBounds(200, 400, 1500, 300); //x,y,size x , size y
 		resultNamePanel.setBackground(Color.white);
 		resultNameLabel = new JLabel (formattedResult + "%");
 		resultNameLabel.setForeground(Color.black);
 		resultNameLabel.setFont(resultFont);
-		
 		if(result < 60.0 ) {
 			window.getContentPane().setBackground(Color.red);
 		}
-		
 		else {
 			window.getContentPane().setBackground(Color.green);
 		}
-		
 		resultNamePanel.add(resultNameLabel);
 		con.add(resultNamePanel);	
 		resultNamePanel.setVisible(true);	
--- Rozetyxzavesy/src/Rozetyxzavesy/Rozetyxzavesy.java
+++ Rozetyxzavesy/src/Rozetyxzavesy/Rozetyxzavesy.java
@@ -1,6 +1,9 @@
 package Rozetyxzavesy;
 
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.SwingUtilities;
 
 public class Rozetyxzavesy {
 	
@@ -8,11 +11,9 @@ public class Rozetyxzavesy {
 	
 	// Beginning state of results
 		
-		float leftNum = 75;
-		float rightNum = 60;
+		float leftNum = 0;
+		float rightNum = 0;
 		float result = rightNum / leftNum * 100;	
-		
-		
 		
 	//prints stuff to monitor
 		
@@ -22,17 +23,57 @@ public class Rozetyxzavesy {
 			Thread.sleep(1000);
 			c.leftChange(leftNum); 
 			c.rightChange(rightNum);
+			c.logo();
 			c.setVisible();
 			Thread.sleep(1000);
 			c.result(result);
 			c.setVisible();
 			
-			
 	// responds to mouse events, calls graphics functions
 			
-		Mouse m = new Mouse();
-		
-		Run r = new Run();
+		MouseListener m = new MouseListener() {
+
+			//@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				Graphics n = new Graphics();
+				float leftNum = 0;
+				float rightNum = 0;
+				float result = rightNum / leftNum * 100;
+				
+				if (SwingUtilities.isLeftMouseButton(e)) {
+					
+					leftNum++;
+					n.leftChange(leftNum);
+					n.result(result);
+				}
+				else if (SwingUtilities.isRightMouseButton(e)) {
+					rightNum++;
+					n.leftChange(rightNum);
+					n.result(result);
+				}
+				else if (SwingUtilities.isMiddleMouseButton(e)) {
+					result = 0;
+					n.result(result);
+				}
+			}
+			//@Override
+			public void mousePressed(MouseEvent e) {
+				
+			}
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {				
+			}
+			
+		};
 		
 	}
 }
