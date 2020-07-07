48cf481024e2df1d03c5ba8e9c9c7440ba81a154
kraljan12 <kraljan12@gmail.com>
2020-07-07 16:17:13 +0200

    Add files via upload

--- Rozetyxzavesy/src/Rozetyxzavesy/Graphics.java
+++ Rozetyxzavesy/src/Rozetyxzavesy/Graphics.java
@@ -3,13 +3,27 @@ package Rozetyxzavesy;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Scanner;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
-public class Graphics{
+public class Graphics implements MouseListener{
 	JFrame window;
 	Container con;
 	JPanel leftNamePanel;
@@ -18,34 +32,62 @@ public class Graphics{
 	JLabel rightNameLabel;
 	JPanel resultNamePanel;
 	JLabel resultNameLabel;
+	JPanel tableNamePanel;
+	JLabel tableNameLabel;
 	JLabel logo;
 	ImageIcon icon;
-	Font rzFont = new Font("Arial" , Font.BOLD, 75); 
-	Font resultFont = new Font("Arial" , Font.BOLD, 300);
+	Font rzFont; 
+	Font resultFont;
+	Font tableFont;
+	File results;
 	
-	public void window(float result) {
+	int leftNum;
+	int rightNum;
+	float result;
+	Instant lastTimeRight;
+	Instant lastTimeLeft;
+	
+	public Graphics(int width, int height) throws IOException {
+		results = new File("vysledky.txt");
+		lastTimeLeft = Instant.now();
+		lastTimeRight = Instant.now();
+		leftNum = 0;
+		rightNum = 0;
+		result = 0;
+		rzFont = new Font("Arial" , Font.BOLD, 75); 
+		resultFont = new Font("Arial" , Font.BOLD, 300);
+		tableFont = new Font("Arial" , Font.BOLD, 20);
+		
 		window = new JFrame();
-		window.setSize(1920, 1080);
+		window.setSize(width, height);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.getContentPane().setBackground(Color.white);
 		//window.setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		//window.setUndecorated(true);
 		window.setLayout(null);
 		con = window.getContentPane();
-		//window.setVisible(true);
+		window.addMouseListener(this);
+		logo();
+		leftNumLabel();
+		rightNumLabel();
+		resultLabel();
+		tableLabel();
+		window.setVisible(true);
 	}
+
 	
-	public void logo() {
+	public void logo() throws IOException {
 	  logo = new JLabel();
-	  ImageIcon icon = new ImageIcon("logo.jpg");
+	  BufferedImage img = ImageIO.read(new File("logo.jpg"));
+	  ImageIcon icon = new ImageIcon(img);
 	  logo = new JLabel(icon);
-	  logo.setBounds(1500, 300, 100, 82); //x,y,size x , size y
+	  logo.setBounds(1150, 0, 1000,200); //x,y,size x , size y, x1920 / y1080
 	  con.add(logo);
 	}
 	
-	public void leftChange(float leftNum) {	
+	public void leftNumLabel() {	
 		leftNamePanel = new JPanel();
-		leftNamePanel.setBounds(50, 100, 600, 100); //x,y,size x , size y
+		leftNamePanel.setBounds(50, 100, 600, 100); //x,y,size x , size y, x1920 / y1080
 		leftNamePanel.setBackground(Color.white);
 		leftNameLabel = new JLabel ("ZÁVÌSY : " + leftNum);
 		leftNameLabel.setForeground(Color.black);
@@ -53,11 +95,10 @@ public class Graphics{
 		leftNamePanel.add(leftNameLabel);
 		con.add(leftNamePanel);	
 		leftNamePanel.setVisible(true);
-		//window.setVisible(true);
 	}
-	public void rightChange(float rightNum) {
+	public void rightNumLabel() {
 		rightNamePanel = new JPanel();
-		rightNamePanel.setBounds(50, 900, 600, 100); //x,y,size x , size y
+		rightNamePanel.setBounds(50, 900, 600, 100); //x,y,size x , size y, x1920 / y1080
 		rightNamePanel.setBackground(Color.white);
 		rightNameLabel = new JLabel ("ROZETY : " + rightNum);
 		rightNameLabel.setForeground(Color.black);
@@ -65,10 +106,9 @@ public class Graphics{
 		rightNamePanel.add(rightNameLabel);
 		con.add(rightNamePanel);	
 		rightNamePanel.setVisible(true);
-		//window.setVisible(true);
-		//return rightChangeNum;
 	}
-	public void result(float result) {
+	public void resultLabel() {		
+				
 		String formattedResult = String.format("%.02f", result);
 		resultNamePanel = new JPanel();
 		resultNamePanel.setBounds(200, 400, 1500, 300); //x,y,size x , size y
@@ -76,19 +116,133 @@ public class Graphics{
 		resultNameLabel = new JLabel (formattedResult + "%");
 		resultNameLabel.setForeground(Color.black);
 		resultNameLabel.setFont(resultFont);
+		
+		resultNamePanel.add(resultNameLabel);
+		con.add(resultNamePanel);	
+		resultNamePanel.setVisible(true);	
+	}
+	public void tableLabel() throws FileNotFoundException {
+		tableNamePanel = new JPanel();
+		tableNamePanel.setBounds(1150, 210, 800, 300); //x,y,size x , size y
+		tableNamePanel.setBackground(Color.white);
+		
+		loadTableLabel();
+		
+		tableNameLabel.setForeground(Color.black);
+		tableNameLabel.setFont(tableFont);
+		tableNamePanel.add(tableNameLabel);
+		con.add(tableNamePanel);
+		tableNamePanel.setVisible(true);
+		
+		
+	}
+	private void loadTableLabel() throws FileNotFoundException {
+		String tmpS = "<html>";
+		Scanner sc = new Scanner(results);
+		
+		int i = 0;
+		while(sc.hasNextLine() && i <= 5) {
+			tmpS = tmpS + sc.nextLine() + "<br/>";
+			i++;
+		}
+		tmpS = tmpS + "</html>";  
+		tableNameLabel = new JLabel(tmpS, SwingConstants.CENTER);
+		sc.close();
+	}
+	public void resultChange() {
+		
+		
+		if (leftNum != 0) {	
+			result = (float) rightNum / leftNum * 100;
+			String formattedResult = String.format("%.02f", result);
+			resultNameLabel.setText(formattedResult + "%");
+			
+		}
 		if(result < 60.0 ) {
 			window.getContentPane().setBackground(Color.red);
 		}
 		else {
 			window.getContentPane().setBackground(Color.green);
 		}
-		resultNamePanel.add(resultNameLabel);
-		con.add(resultNamePanel);	
-		resultNamePanel.setVisible(true);	
-		//window.setVisible(true);
 	}
-	public void setVisible() {
-	window.setVisible(true);
+	
+	public void leftChange() {
+		Instant tmpTime = Instant.now();
+		
+		if(Duration.between(lastTimeLeft, tmpTime).toMillis() > 2000) {
+			leftNum++;
+			leftNameLabel.setText("ZÁVÌSY : " + Integer.toString(leftNum));
+			lastTimeLeft = tmpTime;
+		}
+		
+	}
+	
+	public void rightChange() {
+		Instant tmpTime = Instant.now();
+
+		if(Duration.between(lastTimeRight, tmpTime).toMillis() > 2000) {
+			rightNum++;
+			rightNameLabel.setText("ROZETY : " + Integer.toString(rightNum));
+			lastTimeRight = tmpTime;
+		}
+	}
+
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (SwingUtilities.isLeftMouseButton(e)) {
+			
+			
+			leftChange();
+			resultChange();
+		}
+		else if (SwingUtilities.isRightMouseButton(e)) {
+			
+			rightChange();
+			resultChange();
+			}
+		else if (SwingUtilities.isMiddleMouseButton(e)) {
+			try {
+				FileWriter fw = new FileWriter("vysledky.txt", true);
+				String result = String.format("%s vykon: %f.02 %s \n",LocalDateTime.now().toString() , this.result, "%");
+				fw.append(result);
+				fw.close();
+			} catch (IOException e1) {
+				
+				e1.printStackTrace();
+			}
+			
+			
+			result = 0;
+			leftNum = 0;
+			rightNum = 0;
+			leftNameLabel.setText("ZÁVÌSY : " + Integer.toString(leftNum));
+			rightNameLabel.setText("ROZETY : " + Integer.toString(rightNum));
+			resultNameLabel.setText(Float.toString(result));
+			}
+
+	}
+		
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+	
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
--- Rozetyxzavesy/src/Rozetyxzavesy/Rozetyxzavesy.java
+++ Rozetyxzavesy/src/Rozetyxzavesy/Rozetyxzavesy.java
@@ -1,79 +1,13 @@
 package Rozetyxzavesy;
 
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-import javax.swing.SwingUtilities;
+import java.io.IOException;
 
 public class Rozetyxzavesy {
 	
-	public static void main(String[] args) throws InterruptedException {
-	
-	// Beginning state of results
+	public static void main(String[] args) throws InterruptedException, IOException {
 		
-		float leftNum = 0;
-		float rightNum = 0;
-		float result = rightNum / leftNum * 100;	
-		
-	//prints stuff to monitor
-		
-		Graphics c = new Graphics();
+		new Graphics(1920, 1080);
 
-			c.window (result);
-			Thread.sleep(1000);
-			c.leftChange(leftNum); 
-			c.rightChange(rightNum);
-			c.logo();
-			c.setVisible();
-			Thread.sleep(1000);
-			c.result(result);
-			c.setVisible();
-			
-	// responds to mouse events, calls graphics functions
-			
-		MouseListener m = new MouseListener() {
-
-			//@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				Graphics n = new Graphics();
-				float leftNum = 0;
-				float rightNum = 0;
-				float result = rightNum / leftNum * 100;
-				
-				if (SwingUtilities.isLeftMouseButton(e)) {
-					
-					leftNum++;
-					n.leftChange(leftNum);
-					n.result(result);
-				}
-				else if (SwingUtilities.isRightMouseButton(e)) {
-					rightNum++;
-					n.leftChange(rightNum);
-					n.result(result);
-				}
-				else if (SwingUtilities.isMiddleMouseButton(e)) {
-					result = 0;
-					n.result(result);
-				}
-			}
-			//@Override
-			public void mousePressed(MouseEvent e) {
-				
-			}
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				
-			}
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				
-			}
-			@Override
-			public void mouseExited(MouseEvent e) {				
-			}
-			
-		};
+	}	
 		
-	}
 }
